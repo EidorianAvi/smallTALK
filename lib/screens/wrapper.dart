7a1e320d7da4eb7da74fef8781a543da3e0864be
EidorianAvi <reneavila1993@gmail.com>
2020-08-09 17:39:29 -0600

    Scaffold basic home and authentication structure with wrapper

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:small_talk/screens/authenticate/authenticate.dart';
+import 'package:small_talk/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
