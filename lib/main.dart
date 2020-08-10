e0bc0102c45dba9bb28745be4eb902f70d62ae01
EidorianAvi <reneavila1993@gmail.com>
2020-08-09 19:16:15 -0600

    Add provider package and add User stream to auth file. Wrap root widget tree in StreamProvider to moniter user stream.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:small_talk/models/user.dart';
 import 'package:small_talk/screens/wrapper.dart';
+import 'package:small_talk/services/auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
-
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:small_talk/models/user.dart';
 import 'package:small_talk/screens/authenticate/authenticate.dart';
 import 'package:small_talk/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+
+    
     return Authenticate();
   }
 }
