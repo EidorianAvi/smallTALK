214bedddc44b082b1256808ab0f2b15ea4e44bbb
EidorianAvi <reneavila1993@gmail.com>
2020-08-19 01:41:11 -0600

    All of todays android studio commits

---
+++ lib/screens/topics/post_form.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:small_talk/models/user.dart';
+import 'package:small_talk/services/database.dart';
+import 'package:small_talk/shared/constants.dart';
+
+class PostForm extends StatefulWidget {
+
+  String topic;
+  PostForm(this.topic);
+
+  @override
+  _PostFormState createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+  final _formKey = GlobalKey<FormState>();
+  DatabaseService databaseService = new DatabaseService();
+  String _currentPost;
+
+
+  commitPost(userData){
+    
+    Map<String, dynamic> postMap = {
+      "post": _currentPost,
+      "postedBy": userData.username,
+      "posterImage": userData.image,
+      "posterId": userData.uid,
+      "time": DateTime.now().millisecondsSinceEpoch,
+      "isTaken": false,
+    };
+
+    databaseService.createPost(widget.topic, postMap);
+    Navigator.pop(context);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+
+          UserData userData = snapshot.data;
+
+          return snapshot.hasData ?
+
+          Container(
+            height: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                    "Drop a Topic",
+                  style: TextStyle(
+                    color: Colors.red[900],
+                    fontSize: 22.0,
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                    child: Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              decoration: textInputDecoration.copyWith(
+                                  hintText: ""),
+                              onChanged: (val) =>
+                                  setState(() => _currentPost = val),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.add_box,
+                              color: Colors.red[900],
+                            ),
+                            onPressed: () {
+                              commitPost(userData);
+                            },
+                          ),
+                        ],
+                      ),
+                 ),
+              ],
+            ),
+          ) : Container() ;
+        });
+  }
+}
---
+++ lib/screens/topics/topic_form.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:small_talk/models/user.dart';
+import 'package:small_talk/services/database.dart';
+import 'package:small_talk/shared/constants.dart';
+
+class TopicForm extends StatefulWidget {
+
+  @override
+  _TopicFormState createState() => _TopicFormState();
+}
+
+class _TopicFormState extends State<TopicForm> {
+  final _formKey = GlobalKey<FormState>();
+  DatabaseService databaseService = new DatabaseService();
+  String _currentInput;
+
+
+  commitTopic(){
+    databaseService.createTopic(_currentInput);
+    Navigator.pop(context);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+          return Container(
+            height: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Create Topic",
+                  style: TextStyle(
+                    color: Colors.red[900],
+                    fontSize: 22.0,
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: ""),
+                          onChanged: (val) =>
+                              setState(() => _currentInput = val),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.add_box,
+                          color: Colors.red[900],
+                        ),
+                        onPressed: () {
+                          commitTopic();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+}
